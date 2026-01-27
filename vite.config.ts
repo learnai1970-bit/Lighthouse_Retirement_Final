8ff9185911c5e7d39851ce6e707571b0c03529f5
learnai1970-bit <learnai1970@gmail.com>
2026-01-27 22:51:47 +0530

    Update vite.config.ts
    This is for fixing CSS breakage

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // <--- ADD THIS LINE HERE
   plugins: [
     react({
       // This specifically fixes the "React is not defined" error
